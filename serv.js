06fe6255b297ae2d1a1676b70fcf1325f722c094
BraveWang <1146040444@qq.com>
2019-08-23 10:36:56 +0800

    many changes

--- serv.js
+++ serv.js
@@ -3,14 +3,32 @@
 const titbit = require('./lib/titbit');
 
 var app = new titbit({
+    bodyMaxSize: 100000000,
     debug: true,
     useLimit: true,
+    deny : ['10.7.10.144'],
     maxIPRequest: 520,
     peerTime: 2,
     cert : './rsa/localhost-cert.pem',
     key : './rsa/localhost-privkey.pem',
     http2: true,
     showLoadInfo: false,
+    //globalLog: true,
+    logType: 'stdio',
+    pageNotFound: `<!DOCTYPE html>
+        <html>
+            <head>
+                <meta charset="utf-8">
+                <meta name="viewport" content="width=device-width">
+                <title>titbit - not found</title>
+            </head>
+            <body>
+                <div style="margin-top:3.8rem;text-align:center;color:#565758;">
+                    <h1>404 : page not found</h1>
+                </div>
+            </body>
+        </html>
+    `,
 });
 
 //app.add(app.box.bodyparser);
@@ -30,5 +48,36 @@ app.add(async (ctx, next) => {
     console.log('middleware for POST/PUT');
     await next(ctx);
 }, {method: 'POST,PUT'});
+
+app.add(async (ctx, next) => {
+    if (!ctx.isUpload) {
+        return ;
+    }
+    if (!ctx.getFile('image')) {
+        ctx.res.data = 'file not found, please upload with name "image" ';
+        return ;
+    }
+    await next(ctx);
+}, {preg: '/upload'});
+
+var _total_time = 0;
+app.add(async (ctx, next) => {
+    var start_time = Date.now();
+    await next(ctx);
+    var end_time = Date.now();
+    var timing = end_time-start_time;
+    _total_time += timing;
+    console.log(process.pid,ctx.path, `: ${timing}ms, total: ${_total_time}ms`);
+});
+
+router.post('/upload', async c => {
+    try {
+        c.res.data = await c.moveFile(c.getFile('image'), {
+            path : process.env.HOME + '/tmp/buffer',
+        });
+    } catch (err) {
+        c.res.data = err.message;
+    }
+});
 
 app.daemon(1990);
