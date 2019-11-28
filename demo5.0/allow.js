fbd137e97d4df6cab304fc119ac5fb9feecb52ba
BraveWang <1146040444@qq.com>
2019-11-28 15:22:21 +0800

    http2中间件运行在调整请求上下文后的bug修复以及错出处理优化调整

--- demo5.0/allow.js
+++ demo5.0/allow.js
@@ -3,19 +3,29 @@
 const titbit = require('../main');
 
 var app = new titbit({
+    debug : true,
     allow : [
         '127.0.0.1'
     ],
-    maxIPRequest: 1,
+    maxIPRequest: 10,
     peerTime: 5,
     useLimit: true,
     http2: true,
     cert : '../rsa/localhost-cert.pem',
     key : '../rsa/localhost-privkey.pem',
+    showLoadInfo: true,
+    //loadInfoFile : '/tmp/loadinfo.log'
+});
+
+app.use(async (c, next) => {
+    c.cache = true;
+    await next(c);
 });
 
 app.get('/', async c => {
+    throw new Error('test ok error after send');
     c.send('ok');
+    
 });
 
 app.get('/test', async c => {
