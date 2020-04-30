a81e4765aeb9db9777d00a94339e2d2f8be36374
BraveWang <1146040444@qq.com>
2020-05-01 02:03:49 +0800

    node垃圾

--- demo/log.js
+++ demo/log.js
@@ -1,8 +1,16 @@
 const titbit = require('../main');
+const v8 = require('v8');
+const cluster = require('cluster');
 
 process.on('exit', (code) => {
   console.log('EXIT CODE:', code);
 });
+
+if (cluster.isWorker) {
+  setInterval(() => {
+    console.log(v8.getHeapStatistics());
+  }, 15000);
+}
 
 
 var app = new titbit({
@@ -27,8 +35,9 @@ app.httpServ.globalLog = (method, rinfo) => {
 var _key = 'abcdefghijklmnopqrstuvwxyz123456';
 
 app.get('/', async c => {
+  console.log(c);
     c.res.body = 'success';
-});
+},{name:'home', group:'/'});
 
 app.get('/uuid', async c => {
   c.res.body = c.helper.uuid('w_');
@@ -46,6 +55,10 @@ app.get('/decrypt', async c => {
   c.res.body = c.helper.aesDecrypt(c.query.data, _key);
 });
 
+app.get('/sha256', async c => {
+  c.res.body = c.helper.sha256(`${Math.random()}${Date.now()}`);
+});
 
-app.daemon(2021);
+
+app.daemon(2021, 1);
 
