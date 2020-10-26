a061e5f6d858b25e3c6309175bbb054fa5a31e7b
wangyong <1146040444@qq.com>
2020-10-26 21:43:52 +0800

    --

--- cache/minserv.js
+++ cache/minserv.js
@@ -5,9 +5,17 @@ const titbit = require('../main')
 const app = new titbit({
   debug: true,
   //globalLog: true
+  useLimit: false,
+  ctxpool : 3000
 })
 
 app.get('/', async c => {
+  await new Promise((rv, rj) => {
+    setTimeout(() => {
+      rv()
+    }, parseInt(Math.random() * 5) + 100)
+  })
+
   c.send('success')
 })
 
