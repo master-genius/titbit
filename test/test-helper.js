31134e90c385955111bf04c95fe5a24188b351db
wangyong <1146040444@qq.com>
2020-12-10 22:41:36 +0800

    test for helper

--- test/test-helper.js
+++ test/test-helper.js
@@ -9,15 +9,19 @@ let data = JSON.stringify({
   age : 30
 })
 
-let cryptData = helper.aesEncrypt(data, key)
+let iv = 'qwertyuiopasdfgh'
 
-console.log('aes decrypt: ', helper.aesDecrypt(cryptData, key))
+let cryptData = helper.aesEncrypt(data, key, iv)
+
+console.log('encrypt data:', cryptData)
+
+console.log('aes decrypt: ', helper.aesDecrypt(cryptData, key, iv))
 
 console.log('timestr: ', helper.timestr() )
 
 console.log('make name: ', helper.makeName('aswe.jpg'))
 
-console.log('uuid: ', helper.uuid())
+console.log('make salt:', helper.makeSalt())
 
 console.log(helper.ctype('.jppe'))
 
@@ -30,3 +34,39 @@ console.log('hmacsha1: ', helper.hmacsha1('12345', '123456', 'base64'))
 for (let i = 0; i < 5; i++) {
   console.log( helper.nrand(i, 1000 - i) )
 }
+
+for (let i = 0; i < 10 ; i++) {
+  console.log( helper.uuid() )
+}
+
+for (let i = 0; i < 10 ; i++) {
+  console.log( helper.uuid(true) )
+}
+
+let st = Date.now()
+
+let uuidmap = {}
+let tmp
+
+let output_count = 0
+
+let crashcount = 0
+
+for (let i = 0; i < 1000000; i++) {
+  //tmp = helper.uuid(true)
+  tmp = helper.makeId(12)
+  if (uuidmap[tmp] === undefined) {
+    if (output_count < 10) {
+      console.log(tmp)
+      output_count += 1
+    }
+    uuidmap[tmp] = 1
+  } else {
+    crashcount += 1
+  }
+}
+
+let et = Date.now()
+
+console.log(et - st, 'ms', crashcount)
+
